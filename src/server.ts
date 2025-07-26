2d9ddcdc2d48c892da43d2af1ef0f823b388031f
Vipul <007vipulrank1@gmail.com>
2025-07-27 01:59:55 +0530

    temp

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import colors from 'colors'
+import { DB_URI, NODE_PORT } from './config/env.config'
 
 const app = express()
 
-// app.get('/', (req, res) => {
-//   res.send('hii')
-// })
+app.get('/', (req, res) => {
+  res.send('hii')
+})
 
-app.listen(3000, () => {
-  console.log('App listening to requests on port 3000')
+app.listen(NODE_PORT, () => {
+  console.log(
+    colors.white.bgGreen('\t  App listening to requests on port ' + NODE_PORT + '\t  ').underline
+      .bold
+  )
 })
