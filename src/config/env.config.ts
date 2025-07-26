95a7ee1f2d2eb3d289b993cda2b907333d6d5ecd
Vipul <007vipulrank1@gmail.com>
2025-07-27 02:23:34 +0530

    ADD: prisma

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,21 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const NODE_PORT = process.env.NODE_PORT
-const DB_KEY = process.env.DB_KEY
-const DB_URI = process.env.DB_URI
 
 if (!NODE_PORT) {
   throw new Error('NODE_PORT is not defined in the .env file')
 }
 
-if (!DB_KEY) {
-  throw new Error('DB_KEY is not defined in the .env file')
-}
-
-if (!DB_URI) {
-  throw new Error('DB_URI is not defined in the .env file')
-}
-
-DB_URI.replace('%KEY%', DB_KEY)
-
-export { NODE_PORT, DB_URI }
+export { NODE_PORT }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import express from 'express'
 import colors from 'colors'
-import { DB_URI, NODE_PORT } from './config/env.config'
+import { NODE_PORT } from './config/env.config'
+import prisma from './lib/prisma-client'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hii')
+app.get('/health', async (req, res) => {
+  try {
+    await prisma.$connect()
+    console.log('API health is good, db connection successfull')
+    res.status(200).send('API health is good, db connection successfull')
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('API health is not good, db connection failed')
+  }
 })
 
 app.listen(NODE_PORT, () => {
   console.log(
-    colors.white.bgGreen('\t  App listening to requests on port ' + NODE_PORT + '\t  ').underline
-      .bold
+    colors.white.bgGreen('  App listening to requests on port ' + NODE_PORT + '  ').underline.bold
   )
 })
